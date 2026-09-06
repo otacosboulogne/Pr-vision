140c369c84c07bfe2356ce3f384d602cdcb82831
otacosboulogne <217191891+otacosboulogne@users.noreply.github.com>
2026-09-06 21:21:08 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  // Colle ici l'URL /exec de ton déploiement Google Apps Script.
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxhxpo7MPaOa474uHm8eBwbxWkUSj5RXsjhfOzL6QsABcMAfZ6DI9KltTcRlBnMcGm5ew/exec'
-};
