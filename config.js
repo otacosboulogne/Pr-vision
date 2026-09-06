51afca6d15e3490124cd7cca113d54138583804e
otacosboulogne <217191891+otacosboulogne@users.noreply.github.com>
2026-09-06 13:08:33 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// URL du Web App Google Apps Script après déploiement.
-// Laisser la valeur ci-dessous pour tester uniquement en localStorage.
-window.APP_CONFIG = {
-  GAS_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEBAPP_URL_HERE'
-};
